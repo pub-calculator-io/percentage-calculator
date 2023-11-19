9afe38d379df162bb0565210234a10c50d661133
Arseniy Olevskiy <arsidian@gmail.com>
2023-11-19 13:26:47 +0400

    calculator changes commit

--- plugin.php
+++ plugin.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: Percentage Calculator by Calculator.iO
+Plugin Name: Percentage Calculator by www.calculator.io
 Plugin URI: https://www.calculator.io/percentage-calculator/
 Description: This percentage calculator computes the percent of a number, the percentage change, and the quantity of a number whose percentage is provided.
 Version: 1.0.0
@@ -16,7 +16,7 @@ if (!function_exists('add_shortcode')) return "No direct call for Percentage Cal
 
 function display_ci_percentage_calculator(){
     $page = 'index.html';
-    return '<h2><a href="https://www.calculator.io/percentage-calculator/" target="_blank"><img src="' . esc_url(plugins_url('assets/images/icon-48.png', __FILE__ )) . '" width="48" height="48"></a> Percentage Calculator</h2><div><iframe style="background:transparent; overflow: scroll" src="' . esc_url(plugins_url($page, __FILE__ )) . '" width="100%" frameBorder="0" allowtransparency="true" onload="this.style.height = this.contentWindow.document.documentElement.scrollHeight + \'px\';" id="ci_percentage_calculator_iframe"></iframe></div>';
+    return '<h2><img src="' . esc_url(plugins_url('assets/images/icon-48.png', __FILE__ )) . '" width="48" height="48">Percentage Calculator</h2><div><iframe style="background:transparent; overflow: scroll" src="' . esc_url(plugins_url($page, __FILE__ )) . '" width="100%" frameBorder="0" allowtransparency="true" onload="this.style.height = this.contentWindow.document.documentElement.scrollHeight + \'px\';" id="ci_percentage_calculator_iframe"></iframe></div>';
 }
 
 add_shortcode( 'ci_percentage_calculator', 'display_ci_percentage_calculator' );
